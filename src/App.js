e869c0c67ee32e613f4fa6f53f21d59f4a5c6007
Talib <talibmukadam@gmail.com>
2019-12-29 20:04:44 +0700

    added hop page and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ShopPage from "./pages/shop/shoppage";
+import {Switch, Route} from 'react-router-dom'
+
+
 import HomePage from './pages/homepage/HomePage.component'
 import './App.css';
+
+
 
 function App() {
     return (
         <div className="App">
-            <HomePage/>
+            <Switch>
+                <Route exact={true} path={'/'} component={HomePage}/>
+                <Route exact={true} path={'/shop'} component={ShopPage}/>
+            </Switch>
         </div>
     );
 }
